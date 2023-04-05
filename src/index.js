92d0f84568db3ea6e8c61bbf280a30a126994f7a
juliya <juliya@MacBook-Air-juliya.local>
2023-04-05 13:20:10 +0200

    feat: environment configure

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './index.html'
+import './style.css'
+import { cards } from './data/cards.js'
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const mode = process.env.NODE_ENV || 'production'
+
+module.exports = {
+  mode,
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+
+    new CopyPlugin({
+      patterns: [{ from: 'src/data', to: 'data/' }],
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+}
